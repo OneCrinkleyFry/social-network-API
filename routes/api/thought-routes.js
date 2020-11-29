6d4e59e24d3e666a8d7b1f5287efac4c140cf62b
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-11-28 22:40:12 -0700

    added the ability to create a thought

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+const { getAllThoughts, getThoughtById, createThought } = require('../../controllers/thought-controller');
 
+router
+   .route('/')
+   .get(getAllThoughts)
+   .post(createThought)
+
+router
+   .route('/:id')
+   .get(getThoughtById)
 
 module.exports = router;
