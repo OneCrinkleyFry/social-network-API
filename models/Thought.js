27ba04c0698aabc8c227f071d0c70c6a457c1311
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-11-29 20:02:45 -0700

    added comments to the models

--- models/Thought.js
+++ models/Thought.js
@@ -1,61 +1,78 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
+// sub document
 const ReactionSchema = new Schema(
    {
+      // creates new id on initialization of a reaction
       reactionId: {
          type: Schema.Types.ObjectId,
          default: () => new Types.ObjectId()
       },
+      // The actual text
       reactionBody: {
          type: String,
          required: true,
+         // limits charaters to 280
          max: 280
       },
+      // The username of the creator
       username: {
          type: String,
          required: true
       },
+      // timestamp
       createdAt: {
          type: Date,
          default: Date.now,
          get: (createdAtVal) => {
+            // formats the date
             return moment(createdAtVal).format('M/D h:m a');
          }
       }
    }
 )
 
+//Thought Model
 const ThoughtSchema = new Schema(
    {
+      // The text of the thought
       thoughtText: {
          type: String,
          required: true,
+         // limits characters between 1, and 280
          min: 1,
          max: 280
       },
+      // timestamp
       createdAt: {
          type: Date,
          default: Date.now,
          get: (createdAtVal) => {
+            // formats the date
             return moment(createdAtVal).format('M/D h:m a');
          }
       },
+      // username of the creator
       username: {
          type: String,
          required: true
       },
+      // an array containing reactions that follow the reaction sub document
       reactions: [ReactionSchema]
    },
    {
       toJSON: {
+         // allows getters and the use of virtuals
          getters: true,
          virtuals: true
       },
+      // does not create a redundant id
       id: false
    }
 );
 
+// a virtual to count the total reactions in the thoughts reactions array
 ThoughtSchema.virtual('reactionCount').get(function() {
    return this.reactions.length;
 });
--- models/User.js
+++ models/User.js
@@ -2,25 +2,30 @@ const { Schema, model } = require('mongoose');
 const Thought = require('./Thought');
 
 const UserSchema = new Schema(
-   {
+   {  
+      // the username for a user
       username: {
          type: String,
          unique: true,
          required: true,
          trim: true
       },
+      // the user's email
       email: {
          type: String,
          required: true,
          unique: true,
+         // regex to pick out a valid email.
          match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Please enter a valid email address']
       },
+      // an array of thoughts 
       thoughts: [
          {
             type: Schema.Types.ObjectId,
             ref: 'Thought'
          }
       ],
+      // an array of other users
       friends: [
          {
             type: Schema.Types.ObjectId,
@@ -30,21 +35,27 @@ const UserSchema = new Schema(
    },
    {
       toJSON: {
+         // allows getters, and virtuals
          virtuals: true,
          getters: true
       },
+      //removes the redundant id
       id: false
    }
 );
 
+// a virtual to count the number of friends
 UserSchema.virtual('friendCount').get(function () {
    return this.friends.length;
 });
 
+// a middleware function that is called after a find and delete method is called.
 UserSchema.post('findOneAndDelete', function(doc) {
+   //assigns thoughts to the erased user document, thoughts array
    const thoughts = doc.thoughts;
-   console.log(`-----------${thoughts}`);
+   // iterates throught the thoughts array
    for (let i = 0; i < thoughts.length; i++) {
+      // removes any the thought by id
       Thought.findOneAndDelete({ _id: thoughts[i] })
       .then(dbThoughtData => {
          if (!dbThoughtData) {
