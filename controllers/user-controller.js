ee35e3cbda03d376a7e6f8ee435fbbbb4c8c7211
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-11-29 19:51:32 -0700

    added the comments to the user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,16 +3,23 @@ const { User } = require('../models');
 const userController = {
    // get all users
    getAllUsers(req, res) {
+      // finds all users
       User.find({})
+         // "joins" the collections
          .populate({
+            //at thoughts
             path: 'thoughts',
             select: 'thoughtText createdAt reactions reactionCount'
          })
          .populate({
+            // at friends
             path: 'friends',
+            // doesn't display __v
             select: '-__v'
          })
+         // doesn't display __v
          .select('-__v')
+         // sorts by newest
          .sort({ _id: -1 })
          .then(dbUserData => res.json(dbUserData))
          .catch(err => {
@@ -23,14 +30,20 @@ const userController = {
    //get a single user by _id
    getUserById({ params }, res) {
       User.findOne({ _id: params.id })
+         // "joins" the collections
          .populate({
+            //at thoughts
             path: 'thoughts',
+            // doesn't display __v
             select: '-__v'
          })
          .populate({
+            // at friends
             path: 'friends',
+            // doesn't display __v
             select: '-__v'
          })
+         // doesn't display __v
          .select('-__v')
          .then(dbUserData => {
             if (!dbUserData) {
@@ -53,6 +66,7 @@ const userController = {
    },
    //update a user
    updateUser({ params, body }, res) {
+      // finds a user by id, and then updates the field in the body
       User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
          .then(dbUserData => {
             if (!dbUserData) {
@@ -65,6 +79,7 @@ const userController = {
    },
    //delete a user
    deleteUser({ params }, res) {
+      // finds a user by id, and removes it from the document
       User.findByIdAndDelete(params.id)
          .then(dbUserData => {
             if (!dbUserData) {
@@ -77,35 +92,39 @@ const userController = {
    },
    // add a friend
    addFriend({ params }, res) {
+      //finds the user 
       User.findOneAndUpdate(
          { _id: params.userId },
+         // and adds a reference to another user
          { $push: { friends: params.friendId } },
          { new: true }
       )
-      .then(dbUserData => {
-         if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id!' });
-            return;
-         }
-         res.json(dbUserData);
-      })
-      .catch(err => res.json(err));
+         .then(dbUserData => {
+            if (!dbUserData) {
+               res.status(404).json({ message: 'No user found with this id!' });
+               return;
+            }
+            res.json(dbUserData);
+         })
+         .catch(err => res.json(err));
    },
    // remove friend
    removeFriend({ params }, res) {
+      //finds the user
       User.findOneAndUpdate(
          { _id: params.userId },
+         //and pulls the reference to another user out of the friends array
          { $pull: { friends: params.friendId } },
          { new: true }
       )
-      .then(dbUserData => {
-         if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id!' });
-            return;
-         }
-         res.json(dbUserData);
-      })
-      .catch(err => res.json(err));
+         .then(dbUserData => {
+            if (!dbUserData) {
+               res.status(404).json({ message: 'No user found with this id!' });
+               return;
+            }
+            res.json(dbUserData);
+         })
+         .catch(err => res.json(err));
    }
 }
 
